9585734416b11c3bb279f0ab90b57585e73e25e6
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-04-26 12:02:47 +0100

    created mongoDB cluster and connected express server to it

--- server/index.js
+++ server/index.js
@@ -2,10 +2,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require('mongoose');
+const { config } = require("dotenv");
+config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully!');
+});
 
 app.listen(5005, () => {
   console.log("Server has started on port 5005!");
