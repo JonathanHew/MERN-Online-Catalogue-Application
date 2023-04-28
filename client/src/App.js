bb99c101dfd83643ea2ce9eb88f4169eeafc0b04
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-04-28 11:51:50 +0100

    implemented react-router-dom, created a main page and a error page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import { Fragment } from 'react';
-import './App.css';
+import { Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./Pages/MainPage";
+import ErrorPage from "./Pages/ErrorPage";
+import "./App.css";
 
 function App() {
   return (
     <Fragment>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<MainPage />} />
+          <Route path="/*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
     </Fragment>
   );
 }
