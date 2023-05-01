7f12fab75fa0d477ee819095614d00398f9fdebc
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-05-01 16:05:01 +0100

    Added link on Error Page to home page

--- client/src/Pages/ErrorPage.js
+++ client/src/Pages/ErrorPage.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <Fragment>
+      <div>
+        <h1>Error: Page not found</h1>
+        <Link to="/">Go to the main page</Link>
+      </div>
+    </Fragment>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
